23eeb617da5656d900bd7157296d4927af28ad35
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-24 11:21:08 +0530

    init

--- src/test/java/com/example/TestingApplication/TestingApplication/services/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/example/TestingApplication/TestingApplication/services/impl/EmployeeServiceImplTest.java
@@ -4,6 +4,7 @@ import com.example.TestingApplication.TestingApplication.TestContainerConfig;
 import com.example.TestingApplication.TestingApplication.dto.EmployeeDto;
 import com.example.TestingApplication.TestingApplication.entities.Employee;
 import com.example.TestingApplication.TestingApplication.repositories.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -34,28 +36,56 @@ class EmployeeServiceImplTest {
     @InjectMocks
     private EmployeeServiceImpl employeeService;
 
+    private Employee mockEmployee;
+    private EmployeeDto mockEmployeeDto;
+
+    @BeforeEach
+    void setUp() {
+
+        mockEmployee = Employee
+                .builder()
+                .id(1L)
+                .name("Deep")
+                .email("deep@gmail.com")
+                .salary(200L)
+                .build();
+
+        mockEmployeeDto = modelMapper.map(mockEmployee, EmployeeDto.class);
+
+    }
+
 
     @Test
     void testGetEmployeeById_WhenEmployeeIdIsPresent_ThenReturnEmployeeDto() {
         //assign
-        Long id = 1L;
-        Employee mockEmployee = Employee.builder().id(id).name("Deep").email("deep@gmail.com").salary(200L).build();
+        Long id = mockEmployee.getId();
+
         when(employeeRepository.findById(id)).thenReturn(Optional.of(mockEmployee)); //stubbing
 
         //act
-
         EmployeeDto employeeDto = employeeService.getEmployeeById(id);
 
         //assert
         assertThat(employeeDto.getId()).isEqualTo(id);
         assertThat(employeeDto.getEmail()).isEqualTo(mockEmployee.getEmail());
-        verify(employeeRepository,only()).findById(id);
+        verify(employeeRepository, only()).findById(id);
 
 
     }
 
     @Test
     void testCreateNewEmployee_WhenValidEmployee_ThenCreateNewEmployee() {
+        //assign
+        when(employeeRepository.findByEmail("deep@gmail.com")).thenReturn(List.of());
+        when(employeeRepository.save(any(Employee.class))).thenReturn(mockEmployee);
+
+        //act
+        EmployeeDto employeeDto = employeeService.createNewEmployee(mockEmployeeDto);
+
+        //assert
+
+        assertThat(employeeDto).isNotNull();
+        assertThat(employeeDto.getEmail()).isEqualTo(mockEmployeeDto.getEmail());
 
     }
 
