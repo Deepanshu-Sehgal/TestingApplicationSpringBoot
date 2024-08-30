dd09fc03bebc4f6f827e6c8e0447caed04e7a0ff
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-30 18:23:03 +0530

    init

--- src/main/java/com/example/TestingApplication/TestingApplication/TestingApplication.java
+++ src/main/java/com/example/TestingApplication/TestingApplication/TestingApplication.java
@@ -1,13 +1,22 @@
 package com.example.TestingApplication.TestingApplication;
 
+import com.example.TestingApplication.TestingApplication.services.DataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class TestingApplication {
+public class TestingApplication implements CommandLineRunner {
 
+	private final DataService dataService;
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Data is :" + dataService.getData());
+	}
 }
---
+++ src/main/java/com/example/TestingApplication/TestingApplication/services/DataService.java
@@ -0,0 +1,5 @@
+package com.example.TestingApplication.TestingApplication.services;
+
+public interface DataService {
+    String getData();
+}
